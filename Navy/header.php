bac78d21d4602227ebad00b53c0eb8268be19708
VxRain <i@vxblue.com>
2017-10-08 11:09:22 +0800

    前端开源库CDN改为https..

--- Navy/header.php
+++ Navy/header.php
@@ -14,11 +14,11 @@
             'author'    =>  _t('%s 发布的文章')
         ), '', ' - '); ?><?php $this->options->title(); ?></title>
 	<!--[if lt IE 9]>
-    <script type="text/javascript" src="http://cdn.staticfile.org/html5shiv/r29/html5.min.js"></script>
+    <script type="text/javascript" src="https://cdn.staticfile.org/html5shiv/r29/html5.min.js"></script>
     <![endif]-->
-    <link rel="stylesheet" href="http://cdn.staticfile.org/normalize/3.0.1/normalize.min.css">
+    <link rel="stylesheet" href="https://cdn.staticfile.org/normalize/3.0.1/normalize.min.css">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css'); ?>">
-    <script type="text/javascript" src="http://cdn.staticfile.org/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.staticfile.org/jquery/1.11.0/jquery.min.js"></script>
     <?php $this->header("generator=&template=&"); ?>
 </head>
 <body>
